a82f68a457f98717428e0f85d6e303d14f3a94e5
buivuongFreelance <buivuongdhmo@gmail.com>
2022-05-07 17:40:45 +0700

    .

--- model/src/index.js
+++ model/src/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import router from './router';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+app.use(cors);
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
