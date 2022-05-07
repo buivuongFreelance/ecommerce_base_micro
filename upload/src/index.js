5932897bcf85baf9a87d40ee1d7e54686be5ebd2
buivuongFreelance <buivuongdhmo@gmail.com>
2022-05-07 18:06:39 +0700

    .

--- upload/src/index.js
+++ upload/src/index.js
@@ -2,16 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import router from './router';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 app.use('/api/v1/upload', router);
 
-app.listen(3000, () => {
+app.listen(30001, () => {
   // eslint-disable-next-line no-console
-  console.log('app is listening to port 3000');
+  console.log('app is listening to port 30001');
 });
