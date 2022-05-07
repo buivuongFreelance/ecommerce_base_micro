12156de0c9f6c6c484671bfd910f0ae04c75919a
buivuongFreelance <buivuongdhmo@gmail.com>
2022-05-07 17:36:10 +0700

    .

--- model/src/index.js
+++ model/src/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/api/v1/model', router);
 
-app.listen(3000, () => {
+app.listen(20006, () => {
   // eslint-disable-next-line no-console
-  console.log('app is listening to port 3000');
+  console.log('app is listening to port 20006');
 });
